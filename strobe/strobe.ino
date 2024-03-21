ab3669e38498c48cfd2459487d9c8043cc98371a
Dmitriy Andreev <dmtrii.a@gmail.com>
2024-03-21 15:02:24 +0300

    check button work

--- strobe/strobe.ino
+++ strobe/strobe.ino
@@ -1,5 +1,5 @@
 
-const int buttonPin = 2;  // button pin IN - D2
+const int buttonPin = 2;  // button pin IN - D2, open - 1, closed - 0
 
 const int lamp_1 = 3;  // pin of lamp 1 - D3
 const int lamp_2 = 4;  // pin of lamp 1 - D4
@@ -7,8 +7,17 @@ const int lamp_3 = 5;  // pin of lamp 1 - D5
 const int lamp_4 = 6;  // pin of lamp 1 - D6
 
 int buttonState = 0;  // variable for switching modes
+unsigned long lastTimePressed;
+int btnPressed = 700;  // time for anti-bounce
+
+int cnt = 0;
 
 void pressButton() {
+  if (millis() - lastTimePressed > btnPressed) {
+    lastTimePressed = millis();
+    buttonState++;
+    if (buttonState > 5) buttonState = 0;
+  }
 }
 
 void setup() {
@@ -18,9 +27,18 @@ void setup() {
   pinMode(lamp_2, OUTPUT);
   pinMode(lamp_3, OUTPUT);
   pinMode(lamp_4, OUTPUT);
-  pinMode(buttonPin, INPUT);
-  attachInterrupt(0, pressButton, CHANGE);
+  lastTimePressed = millis();
+  attachInterrupt(0, pressButton, FALLING);
 }
 
 void loop() {
+  Serial.print("button: ");
+  Serial.print(digitalRead(buttonPin));
+  Serial.print("    state: ");
+  Serial.println(buttonState);
+  cnt++;
+  if (cnt > 100) {
+    Serial.println();
+    cnt = 0;
+  }
 }
