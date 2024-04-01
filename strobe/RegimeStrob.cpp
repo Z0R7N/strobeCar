fd3a3116455492c33c140acc8d84f455837d444a
Dmitriy Andreev <dmtrii.a@gmail.com>
2024-04-01 06:03:38 +0300

    update class

--- strobe/RegimeStrob.cpp
+++ strobe/RegimeStrob.cpp
@@ -8,17 +8,19 @@ RegimeStrob::RegimeStrob(int timeStrob, bool lamp1, bool lamp2, bool lamp3, bool
   _lamp4 = lamp4;
 }
 
-// class RegimeStrob {
-// public:
-//   // int getTimeStrob() return _timeStrob;
-//   // bool getLamp1() return _lamp1;
-//   // bool getLamp1() return _lamp2;
-//   // bool getLamp1() return _lamp3;
-//   // bool getLamp1() return _lamp4;
-// private:
-//   int _timeStrob;
-//   bool _lamp1;
-//   bool _lamp2;
-//   bool _lamp3;
-//   bool _lamp4;
-// };
+int RegimeStrob::getTimeStrob() {
+  return _timeStrob;
+}
+
+bool RegimeStrob::getLamp1() {
+  return _lamp1;
+}
+bool RegimeStrob::getLamp2() {
+  return _lamp2;
+}
+bool RegimeStrob::getLamp3() {
+  return _lamp3;
+}
+bool RegimeStrob::getLamp4() {
+  return _lamp4;
+}
